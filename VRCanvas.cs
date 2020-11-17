27c6e7abef752430cb88f8aef1709ee314148471
unknown <micposso@gmail.com>
2020-11-17 06:28:51 -0500

    add buttons scripts

---
+++ VRCanvas.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCanvas : MonoBehaviour
+{
+
+    private GazeableButton currentActiveButton;
+
+    public Color unselectedColor = Color.white;
+    public Color selectedColor = Color.green;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetActiveButton(GazeableButton activeButton)
+    {
+      if (currentActiveButton != null)
+          {
+            currentActiveButton.SetButtonColor(unselectedColor);
+          }
+
+      if(activeButton != null && currentActiveButton != activeButton) 
+        {
+            currentActiveButton = activeButton;
+            currentActiveButton.SetButtonColor(selectedColor);
+        } else {
+            Debug.Log("resetting");
+        currentActiveButton = null;
+        }
+    }
+}
